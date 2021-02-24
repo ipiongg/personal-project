d322eadf6b407837d53f863806bdec387ca625fb
Lucas Costa <olucascosta98@gmail.com>
2021-02-23 22:08:41 -0300

    feat: Add struture project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:personal_portfolio/app/app_widget.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Container(color: Colors.amber),
-    );
-  }
+  runApp(AppWidget());
 }
