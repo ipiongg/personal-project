12454c90be35ff36d5df7d4acf35f2869ad4b019
Lucas Costa <olucascosta98@gmail.com>
2021-02-28 14:34:38 -0300

    feat: Add modularApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:personal_portfolio/app/app_widget.dart';
 
-void main() {
-  runApp(AppWidget());
-}
+import 'app/app_module.dart';
+
+void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
