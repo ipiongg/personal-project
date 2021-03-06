64fd0ca3510ed30925ae6af9500043b174a65d09
Lucas Costa <olucascosta98@gmail.com>
2021-03-06 14:11:08 -0300

    feat: Atualiza para flutter 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:personal_portfolio/app/app_widget.dart';
-
 import 'app/app_module.dart';
 
 void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
